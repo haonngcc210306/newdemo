23d5bbbc214fe9238926e041c83fe1c015485444
haonngcc210306 <135189956+haonngcc210306@users.noreply.github.com>
2023-10-23 11:17:48 +0700

    new commit

---
+++ connect.php
@@ -0,0 +1,40 @@
+<?php
+class connect{
+    public $server;
+    public $dbName;
+    public $username;
+    public $password;
+
+    public function __construct(){
+        $this->server ="jtb9ia3h1pgevwb1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $this->username ="lss2o3fjo4fqphgx";
+        $this->password ="azghj3xwx7vvugf1";
+        $this->dbName="vbfv3tifavmzrqq8";
+    }
+    //option 1: mysqli
+    function connectToMySQL():mysqli{
+        $conn = new mysqli($this->server,$this->username,$this->password,$this->dbName);
+
+        if($conn->connect_error){
+            die("Failed".$conn->connect_error);
+        }else{
+            //echo "Connect!";
+        }
+        return $conn;
+    }
+    //opinion 2 : PDO
+    function connectToPDO():PDO{
+        try{
+            $conn = new PDO("mysql:host=$this->server;dbname=$this->dbName",$this->username,$this->password);
+            //echo "Connect! PDO";
+        }catch(PDOException $e){
+            die("Failed".$e);
+        }
+        return $conn;
+    }
+}
+    // $c = new Connect();
+    // $c->connectToPDO();
+
+//$c->connectToMySQL();
+?>
